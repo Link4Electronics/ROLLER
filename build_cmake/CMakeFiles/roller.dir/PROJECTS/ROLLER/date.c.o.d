492d988def894cc77f6de8046ce493831f08fc45
Link4Electronics <link4electronics@gmail.com>
2026-05-25 21:57:33 -0300

    Delete build_cmake directory

--- build_cmake/CMakeFiles/roller.dir/PROJECTS/ROLLER/date.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/roller.dir/PROJECTS/ROLLER/date.c.o: \
- /run/media/orestes/Guinea_pig/Projetos/ROLLER/PROJECTS/ROLLER/date.c \
- /usr/include/stdc-predef.h \
- /run/media/orestes/Guinea_pig/Projetos/ROLLER/PROJECTS/ROLLER/date.h
